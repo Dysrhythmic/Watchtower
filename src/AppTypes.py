63f99c8b885f38f6484ef66edfff3b31e6cf60bb
Dysrhythmic <jaketucker7@gmail.com>
2025-11-08 23:45:25 -0600

    review constant files

---
+++ src/AppTypes.py
@@ -0,0 +1,10 @@
+"""
+Application and platform type constants.
+
+These constants define the supported platform types used throughout the application
+for both source types (where messages originate) and destination types (where messages are sent).
+"""
+
+APP_TYPE_TELEGRAM = 'telegram'
+APP_TYPE_DISCORD = 'discord'
+APP_TYPE_RSS = 'rss'
